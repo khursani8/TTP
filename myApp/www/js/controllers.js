17edfb3f98950990343a001125b9697bb80c1417
sani <khursani8@gmail.com>
2017-04-03 22:00:12 +0800

    my profile

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -54,12 +54,15 @@ function ($scope, $stateParams,$http,$cordovaGeolocation,login,$state) {
 
 }])
    
-.controller('myProfileCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('myProfileCtrl', ['$scope', '$stateParams','login', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams,login) {
     
-
+    login.getUser()
+        .then(user=>{
+            $scope.user = user.data;
+        })
 }])
  
 .controller('extendParkingCtrl', ['$scope', '$stateParams','$http','login', '$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
